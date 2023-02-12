c3632aeb29c9b6b58c20026af3426c6483d56b52
Kellian <kellian.cottart@edu.ece.fr>
2023-02-12 11:57:23 +0100

    init: added files from rainbow (miss segment_tree)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# RL-RAINBOW
+# Implementation of Rainbow: Combining Improvements in Deep Reinforcement Learning
+
 This is a Reinforcement Learning porject from two students of Ecole Polytechnique / IP Paris.
+
+## Authors
+ 
+| First Name | Last Name  | E-mail address                    |
+| ---------- | ---------- | --------------------------------- |
+| Kellian    | COTTART    | kellian.cottart@polytechnique.edu |
+| Luc        | GENSBITTEL | luc.gensbittel@polytechnique.edu  |
+
+## Bibliography
+
+[1] "Rainbow: Combining Improvements in Deep Reinforcement Learning", Hessel et al., 2017, https://arxiv.org/pdf/1710.02298.pdf  
+[2] "Rainbow Is All you Need", https://github.com/Curt-Park/rainbow-is-all-you-need/
---
+++ main.py
@@ -0,0 +1,71 @@
+import base64
+import glob
+import io
+import os
+import random
+
+import gym
+import numpy as np
+import torch
+from IPython.display import HTML, display
+
+from classes import DQNAgent
+
+
+def seed_torch(seed):
+    torch.manual_seed(seed)
+    if torch.backends.cudnn.enabled:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+
+def show_latest_video(video_folder: str) -> str:
+    """Show the most recently recorded video from video folder."""
+    list_of_files = glob.glob(os.path.join(video_folder, "*.mp4"))
+    latest_file = max(list_of_files, key=os.path.getctime)
+    ipython_show_video(latest_file)
+    return latest_file
+
+
+def ipython_show_video(path: str) -> None:
+    """Show a video at `path` within IPython Notebook."""
+    if not os.path.isfile(path):
+        raise NameError("Cannot access: {}".format(path))
+
+    video = io.open(path, "r+b").read()
+    encoded = base64.b64encode(video)
+
+
+if __name__ == "__main__":
+
+    # environment
+    env_id = "CartPole-v0"
+    env = gym.make(env_id)
+    # seed
+    seed = 777
+    np.random.seed(seed)
+    random.seed(seed)
+    seed_torch(seed)
+    env.seed(seed)
+    # parameters
+    num_frames = 20000
+    memory_size = 10000
+    batch_size = 128
+    target_update = 100
+    # train
+    agent = DQNAgent(env, memory_size, batch_size, target_update)
+    agent.train(num_frames)
+    # test
+    video_folder = "videos/rainbow"
+    agent.test(video_folder=video_folder)
+
+    display(HTML(
+        data="""
+        <video width="320" height="240" alt="test" controls>
+        <source src="data:video/mp4;base64,{0}" type="video/mp4"/>
+        </video>
+        """.format(encoded.decode("ascii"))
+    ))
+
+    latest_file = show_latest_video(video_folder=video_folder)
+    print("Played:", latest_file)
